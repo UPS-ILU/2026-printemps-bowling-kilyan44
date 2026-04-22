c7e4482db3484c4e63148d3105c3b7531287cf13
kilyan44 <k.delhoume@gmail.com>
2026-04-22 18:19:53 +0200

    ITER2

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -1,5 +1,34 @@
 package tdd;
 
-public class GameTest {
+import static org.junit.Assert.assertEquals;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class GameTest {
+	Game game;
+
+	@BeforeEach
+	void setUp() throws Exception{
+		game = new Game();
+	}
+	
+	@Test
+	void iter1() {
+		assertEquals(0,game.score());		
+	}
+	
+	@Test
+	void iter2( ) {
+		roll(20);
+		assertEquals(0,game.score());
+	}
+	
+	private void roll(int i) {
+		for (int j = 0; j < i; j++) {
+			game.roll(i);
+		}
+	}
 }
